8f2cedc8a269ad6aafa56ecd41fa76462c42a32c
yiwenchen0331 <42505192+yiwenchen0331@users.noreply.github.com>
2020-02-10 15:29:13 -0500

    Part1 Implementation

---
+++ Assignment_2_Files/Assignment2_p2.m
@@ -0,0 +1,26 @@
+close all; clear all; clc;
+format compact;
+image = imread('peppers.tif');
+Q = [16 11 10 16 24 40 51 61;
+12 12 14 19 26 58 60 55;
+14 13 16 24 40 57 69 56;
+14 17 22 29 51 87 80 62;
+18 22 37 56 68 109 103 77;
+24 35 55 64 81 104 113 92;
+49 64 78 87 103 121 120 101;
+72 92 95 98 112 100 103 99]
+
+[r,c] = size(image);
+Text = zeros(r*c/64,64);
+for i = 1:r/8
+    for j = 1:c/8
+        block = image((i-1)*8+1:i*8,(j-1)*8+1:j*8);
+        DCT = dct2(block);
+        Q_DCT = DCT./Q;
+        zVec = ZigzagMtx2Vector(Q_DCT);
+        Text(8*8*(i-1)+j,:) = zVec;
+    end
+end
+Len=JPEG_entropy_encode(r,c,8,Q,Text,'',0);
+
+[rowN,colN,dct_block_size,iQ,iZZDCTQIm]=JPEG_entropy_decode('');
---
+++ Assignment_2_Files/ImgCompress.m
@@ -0,0 +1,26 @@
+function [peaksnr] = ImgCompress(PATH,Q_factor)
+
+Image = imread(PATH); % read original image
+Image_size = dir(PATH).bytes; % get original size 
+Name = strsplit(PATH,'.'); %get the image name by string operation
+C_Name = string(Name(1))+num2str(Q_factor)+'.jpeg';%target file name
+% create compressed version of image with quality factor
+imwrite(Image, C_Name, 'Quality', Q_factor); 
+% use PSNR to measure the fidelity of image
+C_Image = imread(C_Name);
+[peaksnr, snr] = psnr(Image, C_Image);
+C_Image_size = dir(C_Name).bytes; % get compressed size
+fprintf('File: %s\n', string(Name(1)));
+fprintf('Quality Factor: %d\n',Q_factor);
+fprintf('Original Size: %d Bytes\n',Image_size);
+fprintf('Compressed Size: %d Bytes\n',C_Image_size);
+fprintf('The Peak-SNR value is %0.4f\n', peaksnr);
+
+subplot(1,2,1);
+imshow(Image);
+title('original');
+subplot(1,2,2);
+imshow(C_Image);
+title(['Q=',num2str(Q_factor),'   PSNR =', num2str(peaksnr)]);
+end
+
