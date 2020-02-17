cb2b79f30eb2d4858cd32ca7a108dcd23abceb13
yiwen-chen-ooo <yiwenchenruru@gmail.com>
2020-02-17 12:14:57 -0500

    Part1 Report Modified

--- Assingment_3_Files/Assignment3_p1.m
+++ Assingment_3_Files/Assignment3_p1.m
@@ -2,16 +2,17 @@ close all; clear all; clc;
 format compact;
 %the path of image file, will be used
 PATH = {'peppers.tif','baboon.tif','LSBwmk1.tiff','LSBwmk2.tiff','LSBwmk3.tiff'};
-% for j = 1:length(PATH)
-% figure(j)
-% Image = imread(string(PATH(j)));% load the image #j 
-% imgSeperate(Image);
-% end
+
+for j = 1:length(PATH)
+figure(j)
+Image = imread(string(PATH(j)));% load the image #j 
+imgSeperate(Image);
+end
 
 Image = imread('peppers.tif');
 wtmk = imread('Barbara.bmp');
 New = WaterMark(Image, wtmk,3);
-figure(1)
+figure(j+1)
 imshow(Image);
-figure(2)
+figure(j+2)
 imgSeperate(New)
