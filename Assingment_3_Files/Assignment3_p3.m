cfb501b6862683f21aada7195870ae5200a82515
yiwen-chen-ooo <yiwenchenruru@gmail.com>
2020-02-24 17:15:01 -0500

    PART3 ISSU CLS

--- Assingment_3_Files/Assignment3_p3.m
+++ Assingment_3_Files/Assignment3_p3.m
@@ -17,25 +17,25 @@ figure_num = figure_num +1;
 figure(figure_num);
 imgSeperate(LSBCombined);
 
+%% Yeung-Mintzer watermarked image
+Combined = [peppers(257:512,:);baboon0(257:512,:)]; % image combine 
+WaterMarkExtracted = YMdecode(Combined,0); %Assume we know the key
 
-% Combined = [peppers(257:512,:);baboon0(257:512,:)]; % image combine 
-% WaterMarkExtracted = YMdecode(Combined,0); %Assume we know the key
-% 
-% figure_num = figure_num + 1;
-% figure(figure_num)
-% subplot(1,2,1)
-% imshow(Combined)
-% title('Peppers Baboons0')
-% subplot(1,2,2)
-% imshow(WaterMarkExtracted);
-% 
-% peppers0 = imread('peppers0.tiff');
-% YMAttack = [peppers0(257:512,:);baboon0(257:512,:)];
-% YMWaterMarkExtracted = YMdecode(YMAttack,0)
-% figure_num = figure_num + 1;
-% figure(figure_num)
-% subplot(1,2,1)
-% imshow(YMAttack)
-% title('Peppers0 Baboons0')
-% subplot(1,2,2)
-% imshow(YMWaterMarkExtracted);
+figure_num = figure_num + 1;
+figure(figure_num)
+subplot(1,2,1)
+imshow(Combined)
+title('Peppers Baboons0')
+subplot(1,2,2)
+imshow(WaterMarkExtracted);
+
+peppers0 = imread('peppers0.tiff');
+YMAttack = [peppers0(257:512,:);baboon0(257:512,:)];
+YMWaterMarkExtracted = YMdecode(YMAttack,0);
+figure_num = figure_num + 1;
+figure(figure_num)
+subplot(1,2,1)
+imshow(YMAttack)
+title('Peppers0 Baboons0')
+subplot(1,2,2)
+imshow(YMWaterMarkExtracted);
