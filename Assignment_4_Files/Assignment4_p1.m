950e08ede675503cf1b36ff4b85b549df79a50a4
yiwen-chen-ooo <yiwenchenruru@gmail.com>
2020-03-08 21:58:35 -0400

    Add Comment and Img File

--- Assignment_4_Files/Assignment4_p1.m
+++ Assignment_4_Files/Assignment4_p1.m
@@ -1,7 +1,9 @@
 close all; clear all; clc;
 format compact;
 
-PATH = {'imageOrigin1.JPG','blockArtifacts1.tif'};
+PATH = {'imageOrigin1.JPG','imageOrigin2.jpg','imageOrigin3.JPG',...
+    'imageOrigin4.JPG','imageOrigin5.jpg','imageOrigin6.jpg',...
+    'blockArtifacts1.tif','blockArtifacts2.tif','blockArtifacts3.tif'};
 
 for i=1:length(PATH)
     figure(i)
