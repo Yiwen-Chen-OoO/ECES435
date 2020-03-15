95cd819a1ad94b079e6d966f22d0d662f6d10385
yiwenchen0331 <42505192+yiwenchen0331@users.noreply.github.com>
2020-03-15 15:58:57 -0400

    Part I

--- Assignment_5_Files/GammaCorrection.m
+++ Assignment_5_Files/GammaCorrection.m
@@ -1,4 +1,5 @@
-function [NewImg] = GammaCorrection(img,gamma)
+function [NewImg] = GammaCorrection(PATH,gamma)
+    img = imread(PATH);
     [Rows, Cols] = size(img);% get the size of img
     DImg = double(img);% convert img to double
     %update the value pixel by pixel
@@ -8,6 +9,13 @@ function [NewImg] = GammaCorrection(img,gamma)
         end
     end
     NewImg = uint8(DImg); % restore the default img format uint8
+    subplot(1,2,1);
+    imshow(NewImg);
+    title(string(PATH));
+    subplot(1,2,2);
+    [Count, Bin] = imhist(NewImg, 10);
+    bar(Bin, Count);
+    title(['\gamma = ',num2str(gamma)]);
     
     % with this command, the floating point value would be 
     % rounded and the range would be set to [0,255]
