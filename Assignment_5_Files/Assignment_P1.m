84f979f01474119dd5a4a69390921aa050ea8ded
yiwenchen0331 <42505192+yiwenchen0331@users.noreply.github.com>
2020-03-15 17:11:50 -0400

    Close Case

--- Assignment_5_Files/Assignment_P1.m
+++ Assignment_5_Files/Assignment_P1.m
@@ -7,6 +7,7 @@ format compact;
 
 %% 1. Contrast Enhancement
 PATH1 = {'imageCE1.tif','imageCE2.tif','imageCE3.tif','imageCE4.tif'};
+n_bin = 10; %num of bins
 for i = 1:length(PATH1)
     IMG = imread(PATH1{i});
     figure(i);
@@ -14,18 +15,24 @@ for i = 1:length(PATH1)
     imshow(IMG);
     title(string(PATH1{i}));    
     subplot(1,2,2);
-    [Count, Bin] = imhist(IMG,10);
-    bar(Bin,Count);
+    [Count, Bin] = imhist(IMG,n_bin);% get img histogram with 10 bins
+    bar(Bin,Count); %plot histogram using bar
+    
 end
 
 
 %% 2. Gamma Correction
 PATH2 = {'unaltIm1.tif','unaltIm2.tif','unaltIm3.tif'};
 Gamma = {0.7,1,1.3};
+% performing gamma correction to the img. 
+% note that img dont change if gamma = 1;
+
 for k = 1:length(Gamma)
     for j = 1:length(PATH2)
        figure
        NewImg = GammaCorrection(PATH2{j},Gamma{k});
+       %Performing gamma correction to the img with respecte to gamma
+       %value
     end
 end
 
@@ -36,5 +43,6 @@ subplot(1,2,1);
 imshow(IMG5);
 title('imageCE5.tif');
 subplot(1,2,2);
-[Count, Bin] = imhist(IMG,10);
+[Count, Bin] = imhist(IMG,n_bin);
 bar(Bin,Count);
+%get the img histogram and plot as bar 
--- Assignment_5_Files/Assignment_P2.m
+++ Assignment_5_Files/Assignment_P2.m
@@ -4,4 +4,14 @@
 % Part II
 close all; clear all; clc;
 format compact;
+% initializing and set parameters
+PATH = {'resamp1.tif','resamp2.tif','resamp3.tif','resamp4.tif'};
+lamda = 1;
+tau = 2;
+sigma = 1;
+%get p map and freq p map for each img. 
 
+for i = 1: length(PATH)
+    p_map = Kirchner(PATH{i},lamda,tau,sigma);
+    
+end
